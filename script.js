1ea8c7caf1c82b60ef608fc1768db9db40928a90
Gilberto Jr <giljrsantos@gmail.com>
2023-03-05 21:40:08 -0300

    Resetando para sempre que clicar no modal aparecer a pizza grande selecionada, e a quantidade de pizza sempre 1

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let modalQt = 1;
+
 // função anonima
 const c = (el) => document.querySelector(el);
 const cs = (el) => document.querySelectorAll(el);
@@ -20,9 +22,29 @@ pizzaJson.map((item, index) => {
         // recuperando o index da pizza
         let key = e.target.closest('.pizza-item').getAttribute('data-key');
 
+        // resetando a quantidade de pizza para 1 toda vez que abrir um modal
+        modalQt = 1;
+
         c('.pizzaBig img').src = pizzaJson[key].img;
         c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
         c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+        c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
+
+        c('.pizzaInfo--size.selected').classList.remove('selected');
+
+        cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
+
+            // if (sizeIndex == 2) {
+            //     size.classList.add('selected');
+            // }
+            // verificando se tamanho da pizza é [2], se for adiciona a classe .selected
+            sizeIndex == 2 ? size.classList.add('selected') : '';
+
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
+
+        });
+
+        c('.pizzaInfo--qt').innerHTML = modalQt;
 
         c('.pizzaWindowArea').style.opacity = 0;
         c('.pizzaWindowArea').style.display = 'flex'
