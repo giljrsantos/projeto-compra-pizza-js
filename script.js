48ba392d8605e661d133ad4584c89b1b8b50e9c5
Gilberto Jr <giljrsantos@gmail.com>
2023-03-06 20:33:03 -0300

    Ações modal, Ação botão cancelar / fechar o modal

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let modalQt = 1;
 const c = (el) => document.querySelector(el);
 const cs = (el) => document.querySelectorAll(el);
 
-
+// lisatagem das pizzas
 pizzaJson.map((item, index) => {
 
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
@@ -57,3 +57,16 @@ pizzaJson.map((item, index) => {
 
 });
 
+
+// Eventos do modal
+
+const closeModal = () => {
+    c('.pizzaWindowArea').style.opacity = 0;
+    setTimeout(() => {
+        c('.pizzaWindowArea').style.display = 'none';
+    }, 500);
+}
+
+cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item) => {
+    item.addEventListener('click', closeModal);
+})
